3b9b896ba8dd04cc66d586d251dc9faa0e600fbb
Julien Thieffry <julien@thieffry.io>
2025-02-26 15:03:17 +0900

    Revert "Add filthy bucket"
    This reverts commit d22e1057bf12b65a4c5205e8dc6699fb0abad62c.

--- core_stack.py
+++ core_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import Stack
 from aws_cdk.aws_lambda import Code, Function, LayerVersion, Runtime
-from aws_cdk.aws_s3 import Bucket
 from constructs import Construct
 
 
@@ -14,6 +13,8 @@ class MyLambdaStack(Stack):
             layer_version_arn="arn:aws:lambda:us-east-1:017000801446:layer:AWSLambdaPowertoolsPythonV3-python312-x86_64:8",
         )
 
+        self.function_name: str | None = None
+
         Function(
             self,
             "MyFunction",
@@ -22,5 +23,3 @@ class MyLambdaStack(Stack):
             handler="lambda_function.handler",
             layers=[powertools_layer],
         )
-
-        Bucket(self, "MyBucket")
